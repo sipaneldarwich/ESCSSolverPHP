45501ec14a6029a2d70beca78bfdca1c19508f1a
Sipan El Darwich <sipan.eldarwich@gmail.com>
2025-02-04 20:38:13 +0100

    fix not sending response by log message

--- public/index.php
+++ public/index.php
@@ -6,24 +6,24 @@ $ipAddress = $_SERVER['REMOTE_ADDR'];
 
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
-if ($data !== null) {
-    $key = htmlspecialchars($data['key'] ?? "");
-    $hardwareKey = htmlspecialchars($data['hardwareKey'] ?? "");
-
-    if ($key == ""  || $hardwareKey == "") {
-        logMessage('missing post key', $ipAddress,$key,$hardwareKey);
-        exit();
-    }
-} else {
+if ($data === null) {
     logMessage('missing post keys', $ipAddress,'*','*');
-    exit();
+    sendResponseAndExit('missing post keys');
+}
+
+$key = htmlspecialchars($data['key'] ?? "");
+$hardwareKey = htmlspecialchars($data['hardwareKey'] ?? "");
+
+if ($key == ""  || $hardwareKey == "") {
+    logMessage('missing post key', $ipAddress,$key,$hardwareKey);
+    sendResponseAndExit('missing post key');
 }
 
 $fileName = '../keys/' . $key . '.json';
 
 if (!file_exists($fileName)) {
     logMessage('key not found', $ipAddress,$key,$hardwareKey);
-    exit();
+    sendResponseAndExit('key not found');
 }
 $fileContent = file_get_contents($fileName);
 $jsonData = json_decode($fileContent, true);
@@ -36,34 +36,35 @@ if ($currentDate > $expireDate)
 {
     logMessage('date is expired', $ipAddress,$key,$hardwareKey);
     updateKey('date is expired', $fileName, $jsonData,'date is expired');
+    sendResponseAndExit('date is expired');
 }
 
 if (!isset($jsonData['hardwareKey'])) {
     $jsonData['hardwareKey'] = $hardwareKey;
     logMessage('loggedin new hardware', $ipAddress,$key,$hardwareKey);
     updateKey('true', $fileName, $jsonData,'loggedin');
+    sendResponseAndExit('true');
 }
 
 if ($jsonData['hardwareKey'] === $hardwareKey) {
     logMessage('loggedin', $ipAddress,$key,$hardwareKey);
     updateKey('true', $fileName, $jsonData,'loggedin');
+    sendResponseAndExit('true');
 }
 
 logMessage('hardware key not found', $ipAddress,$key,$hardwareKey);
 updateKey('hardware key not found', $fileName, $jsonData,'hardware key not found');
+sendResponseAndExit('hardware key not found');
+
 exit();
 
 function updateKey(string $response, string $fileName, array $jsonData, string $statusLog): void
 {
-    echo sprintf('{"result" : "%s"}', $response);
-
     $currentDate = new DateTime();
     $jsonData['lastLogin'] = sprintf("%s - %s",$currentDate->format('Y-m-d H:i:s'), $statusLog);
 
     $jsonData = json_encode($jsonData, JSON_PRETTY_PRINT);
     file_put_contents($fileName, $jsonData);
-
-    exit();
 }
 
 function logMessage(string $statusLog, string $ip, string $key, string $hardwareKey, ): void
@@ -73,4 +74,10 @@ function logMessage(string $statusLog, string $ip, string $key, string $hardware
     $content = sprintf("%s;%s;%s;%s;%s\n", $currentDate->format('Y-m-d H:i:s'),$statusLog, $ip,$key,$hardwareKey);
 
     file_put_contents($file, $content, FILE_APPEND);
+}
+
+function sendResponseAndExit(string $response)
+{
+    echo sprintf('{"result" : "%s"}', $response);
+    exit();
 }
